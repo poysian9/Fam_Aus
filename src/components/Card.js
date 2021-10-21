232cede471492cceda46ce8f0f11789c0218f457
Ren <antonio.aguilar.r95@gmail.com>
2021-10-21 23:41:17 +1100

    Working on Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Image, Badge } from '@chakra-ui/react';
+import chao from './../assets/chao.jpg'
 
 export default function Card() {
     return (
-        <Box>
-
+        <Box maxW="xs">
+            <a>
+                <Image src={chao}/>
+            </a>
+            
+            <Box mt={4} mx={1}>
+                <Box fontWeight='semibold' as='h4'float='left'>
+                    Family Crewneck
+                </Box>
+                <Box float='right'>
+                    Small
+                </Box>
+            </Box>
+            <Badge borderRadius='full' p={2} colorScheme='teal'>
+                Olive
+            </Badge>
         </Box>
     )
 }
