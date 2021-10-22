6f448655121795ec8e0b1cda336023aaba04eee1
Antonio <antonio.aguilar.r95@gmail.com>
2021-10-22 17:12:06 +1100

    Working on Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import { Box, Image, Badge } from '@chakra-ui/react';
+import { Box, Image, Badge, VStack, Spacer, HStack } from '@chakra-ui/react';
 import chao from './../assets/chao.jpg'
 
 export default function Card() {
     return (
-        <Box maxW="xs">
+        <Box maxW="xs" border='1px' borderRadius='xl' p={3}>
             <a>
                 <Image src={chao}/>
             </a>
             
-            <Box mt={4} mx={1}>
-                <Box fontWeight='semibold' as='h4'float='left'>
-                    Family Crewneck
-                </Box>
-                <Box float='right'>
-                    Small
-                </Box>
-            </Box>
-            <Badge borderRadius='full' p={2} colorScheme='teal'>
-                Olive
-            </Badge>
+            <VStack mt={6} mx={1} align='initial'>
+                <HStack>
+                    <Box fontWeight='semibold' as='h4'>
+                        Family Crewneck
+                    </Box>
+                    <Spacer/>
+                    <Box >
+                        100 FTM
+                    </Box>
+                </HStack>
+                <HStack>
+                    <Badge borderRadius='full' p={2} colorScheme='teal'>
+                        Olive
+                    </Badge>
+                    <Spacer/>
+                    <Box >
+                        Small
+                    </Box>
+                </HStack>
+                
+            </VStack>
+            
         </Box>
     )
 }
